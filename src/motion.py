eea6b197361c93185ef4e2ad0d658fab1c40932c
Lexyo14 <lexyo.2002@gmail.com>
2023-06-12 17:12:43 +0200

    Improved speed of Robert's code, Real time rect!!

--- src/motion.py
+++ src/motion.py
@@ -1,3 +1,9 @@
+"""
+Created on Thu Jun  8 14:14:08 2023
+
+@author: mediolanum
+"""
+
 import numpy as np 
 import cv2
 
@@ -9,7 +15,6 @@ class MotionDetection():
         self.video = video
         self.threshold = np.full(self.shape, initial_treshold)
         self.red = np.full(self.shape, [0,0,255], dtype = np.uint8) 
-        self.show = None
 
     def define_rectangles(self):
         h_n = 20 #How many points in the heihgt
@@ -25,25 +30,26 @@ class MotionDetection():
                 if ij == 0: continue
                # top_left = (width_points[ii-1], height_points[ij-1])
               #  bottom_right = (i,j)
-                self.rectangles.append((width_points[ii-1], height_points[ij-1], i,j)) #top left, bottom right
-                
+               # self.rectangles.append((width_points[ii-1], height_points[ij-1], i,j)) #top left, bottom right
+                self.rectangles.append((width_points[ii-1], height_points[ij-1], i,j))
 
 
 
     def draw_rectangles(self):
-        img = self.video.read()[1]
-        for top_left, bottom_right in self.rectangles:
-            cv2.rectangle(img, top_left, bottom_right, [255,0,0], 3)
-            cv2.imshow("", img)
-            if  cv2.waitKey(10) & 0xFF == ord('q'):
-                break
+        count = 0
+        for u in self.rectangles:
+            top_left, bottom_right = (u[0], u[1]),  (u[2], u[3])
+            if self.moving_rectangles[count] == True: 
+                
+                cv2.rectangle(self.img, top_left, bottom_right, [255,0,0], 3)
+            count += 1
         
 
     def rectangle_moving(self, moving_pixels):
         self.moving_rectangles = []
         for borders in self.rectangles:
-            rectangle = moving_pixels[borders[0]:borders[2], borders[1]:borders[3]]
-            if rectangle.shape[0] != 0 and rectangle.shape[1]!=0:
+                rectangle = moving_pixels[borders[0]:borders[2], borders[1]:borders[3]]
+            #if rectangle.shape[0] != 0 and rectangle.shape[1]!=0:
                 if rectangle.sum()>rectangle.size*0.2:
                     self.moving_rectangles.append(True)
                 else:
@@ -62,40 +68,42 @@ class MotionDetection():
         self.define_rectangles()
         ######starting loop#######
         while True:     
-            img = self.video.read()[1]
+            self.img = self.video.read()[1]
             #cv2.imshow("camera", img)
 
         # 3 find if pixel is moving
-            difference_with_previous = abs(img-previous_image)
+            difference_with_previous = abs(self.img-previous_image)
             changes = difference_with_previous > threshold  #binary mask
 
         # 4 update background
-            updated_background = background*a + (1-a)*img
+            updated_background = background*a + (1-a)*self.img
             background = np.where(changes, updated_background, background) #background if false, updated_background if true
 
         # 5 update threshold
-            difference_with_background = abs(img-background)
+            difference_with_background = abs(self.img-background)
             changes_compared_to_background = difference_with_background > threshold #binary mask
             updated_treshold = threshold*a+(1-a)*(c*difference_with_background)
             threshold = np.where(changes, updated_treshold, threshold)
 
         # 6 detecting moving regions
-            highlight = np.where(changes_compared_to_background, red, img) #(1) red if true else value from camera
+         #   highlight = np.where(changes_compared_to_background, red, img) #(1) red if true else value from camera
             moving_pixels = np.any(changes_compared_to_background, axis = 2)
-            print(changes_compared_to_background)
-            cv2.imshow("changes", highlight) #(1)
-            
-            previous_image = img
+      
+           # cv2.imshow("changes", highlight) #(1)
+            previous_image = self.img
             #cv2.imshow("background", background)
-
+            
             self.rectangle_moving(moving_pixels)
-        
+           
+            self.draw_rectangles()
+            cv2.imshow("", self.img)
             if  cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
 
 
 t = cv2.VideoCapture(0)
-motion = MotionDetection(t, 80, 0.95, 4)
+motion = MotionDetection(t, 50, 0.95, 4)
 motion.fit()
+
 
