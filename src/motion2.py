3572b3b1bb811ed08c85eec15996730b03fc923d
Lexyo14 <lexyo.2002@gmail.com>
2023-06-27 13:00:25 +0200

    Removed Class, now it's just a fit and stop functi

--- src/motion2.py
+++ src/motion2.py
@@ -1,86 +1,77 @@
 import cv2
-from threading import Thread
 
-class Motiondetection():
+cap = False
+fitting = False
+
+def stop():
+    #stops fitting function if it's running
+
+    global cap
+    global fitting
+
+    if fitting == True:
+        fitting = False
+        cv2.destroyAllWindows() # destroys cv2 window
+        cap.release()
+
+
+def fit(video = False, toshow = True):
+    global fitting
+    global cap
+
+    fitting = True
+
+    # By default it will use the camera
+    if video != False:# if a video to use is given, it will use it
+        cap = cv2.VideoCapture(video)
+    else:
+        cap = cv2.VideoCapture(0)
+
+    # initializing first variables
+    prev_frame = cap.read()[1] #first frame
+    prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    prev_frame = cv2.GaussianBlur(prev_frame, (21, 21), 0) #converting to grayscale and blurring
+
+    while fitting == True: #until stop() is called run this
+        # Read the current frame
+        frame = cap.read()[1]
+        
+        # Convert the frame to grayscale and apply blur
+        current_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        current_frame = cv2.GaussianBlur(current_frame, (21, 21), 0)
+        
+        # Calculate the absolute difference between the current and previous frame
+        frame_diff = cv2.absdiff(prev_frame, current_frame)
+        
+        # Apply a threshold to create a binary image
+        _, thresh = cv2.threshold(frame_diff, 30, 255, cv2.THRESH_BINARY)
+
+        # Dilate the thresholded image to fill in the holes
+        dilated = cv2.dilate(thresh, None, iterations=2)
+        
+        # Find contours of the moving objects
+        contours, _ = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        # Iterate over the contours and draw rectangles around the moving objects
+        for contour in contours:
+            if cv2.contourArea(contour) < 500:  # Adjust the minimum contour area as needed
+                continue
+            x, y, w, h = cv2.boundingRect(contour)
+            if toshow:
+                #if the running algorithm has to be showm, draw the rectangles representing moving areas
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        if toshow:
+            # Show the running algorithm
+            cv2.imshow("Motion Detection", frame)
+        
+        # Update the previous frame
+        prev_frame = current_frame
+        
+        # Quit if the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            stop()
+            break
+        
+
     
-    def __init__(self, cvshow = True, uishow = False, video = False):
-        #boolean flags to interact with ui
-        self.uishow = uishow
-        self.cvshow = cvshow
-
-        # Initialize the video capture object
-        if video != False:
-            self.cap = cv2.VideoCapture(video)
-        else:
-            self.cap = cv2.VideoCapture(0)  # 0 represents the default camera
-
-        # Read the first frame
-        self.frame = self.cap.read()[1]
-        self.prev_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        self.prev_frame = cv2.GaussianBlur(self.prev_frame, (21, 21), 0)
-    
-    def stop(self):
-        self.fitting = False
-
-    def fit(self, ui = False):
-        self.uishow = ui
-        self.fitting = True
-        while self.fitting == True:
-            # Read the current frame
-            self.frame = self.cap.read()[1]
-            
-            # Convert the frame to grayscale and apply blur
-            current_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-            current_frame = cv2.GaussianBlur(current_frame, (21, 21), 0)
-            
-            # Calculate the absolute difference between the current and previous frame
-            frame_diff = cv2.absdiff(self.prev_frame, current_frame)
-            
-            # Apply a threshold to create a binary image
-            _, thresh = cv2.threshold(frame_diff, 30, 255, cv2.THRESH_BINARY)
-
-            # Dilate the thresholded image to fill in the holes
-            dilated = cv2.dilate(thresh, None, iterations=2)
-            
-            # Find contours of the moving objects
-            contours, _ = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            
-            # Iterate over the contours and draw rectangles around the moving objects
-            for contour in contours:
-                if cv2.contourArea(contour) < 500:  # Adjust the minimum contour area as needed
-                    continue
-                x, y, w, h = cv2.boundingRect(contour)
-                if self.uishow or self.cvshow:
-                    cv2.rectangle(self.frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            
-            if self.cvshow:
-                # Show the frames
-                cv2.imshow("Motion Detection", self.frame)
-            
-            # Quit if the 'q' key is pressed
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            
-            # Update the previous frame
-            self.prev_frame = current_frame
-
-        # Release the video capture object and close the windows
-        self.cap.release()
-        cv2.destroyAllWindows()
-
-"""t = motiondetection()
-
-status = True
-detect_alg = Thread(target = t.fit)
-detect_alg.start()
-
-c=0
-while status!=False:
-    c+=1
-    print("Hello")
-    if c==1000:
-        status = False
-
-status = False
-detect_alg.join()
-"""
