415af18788c5e6e039f54ce1b7f0b402fe695cc5
ami-sc <github@slmail.ami.sc>
2023-06-30 23:12:00 +0200

    .

--- src/main.py
+++ src/main.py
@@ -3,29 +3,32 @@ import motion2
 import PySimpleGUI as sg
 import cv2
 
+from motionclass import motiondetection
+
 layout = [[sg.Button('START', size=(10, 1), font='Helvetica 14')],
           [sg.Button('STOP', size=(10, 1), font='Helvetica 14')]]
 
 window = sg.Window('Demo Application - OpenCV Integration',
                     layout, location=(800, 400))
 
+motion_detect = motiondetection()
+
+motiondetection = Thread(target = motion_detect.fit, daemon = True)
+
 i = 0
 while True:
 
     event, values = window.read(timeout=20)
-    if event == 'Exit' or event == sg.WIN_CLOSED:
+    if event == 'Exit' or event == sg.WIN_CLOSED or event == "STOP":
+        motion_detect.fitting = False
+        motiondetection.join()
         break
 
     elif event == 'START':
-        motiondetection = Thread(target = motion2.fit, daemon = True)
         motiondetection.start()
 
-    elif event == 'STOP':
-        stopmotiondetection = Thread(target = motion2.stop)
-        stopmotiondetection.start()
-    
     i+=1
     print(i, event)
-        
+
 
 
--- src/motionclass.py
+++ src/motionclass.py
@@ -5,21 +5,21 @@ class motiondetection:
         self.fitting = False
         self.contours = None
 
-    def stop():
+    def stop(self):
         #stops fitting function if it's running
         print("stop function")
-        global fitting
 
-        if fitting == True:
+
+        if self.fitting == True:
             print("fitting = False")
-            fitting = False
+            self.fitting = False
 
 
-    def fit(video = False, toshow = True):
-        global fitting
+    def fit(self, video = False, toshow = True):
+
         print("fitting = True")
-        fitting = True
-        print(fitting)
+        self.fitting = True
+        print(self.fitting)
         print(video)
 
         # By default it will use the camera
@@ -38,52 +38,55 @@ class motiondetection:
         prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
         prev_frame = cv2.GaussianBlur(prev_frame, (21, 21), 0) #converting to grayscale and blurring
 
-        while fitting: #until stop() is called run this
+        while self.fitting: #until stop() is called run this
             # Read the current frame
             print("reading i frame")
             frame = cap.read()[1]
-            
+
             # Convert the frame to grayscale and apply blur
             print("manipulating i frame")
             current_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             current_frame = cv2.GaussianBlur(current_frame, (21, 21), 0)
-            
+
             # Calculate the absolute difference between the current and previous frame
             frame_diff = cv2.absdiff(prev_frame, current_frame)
-            
+
             # Apply a threshold to create a binary image
             _, thresh = cv2.threshold(frame_diff, 30, 255, cv2.THRESH_BINARY)
 
             # Dilate the thresholded image to fill in the holes
             dilated = cv2.dilate(thresh, None, iterations=2)
-            
+
             # Find contours of the moving objects
             self.contours, _ = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            
+
             # Iterate over the contours and draw rectangles around the moving objects
             for contour in self.contours:
                 if cv2.contourArea(contour) < 500:  # Adjust the minimum contour area as needed
                     continue
                 x, y, w, h = cv2.boundingRect(contour)
+
+
+
                 if toshow:
                     #if the running algorithm has to be showm, draw the rectangles representing moving areas
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            
+
             if toshow:
                 # Show the running algorithm
                 cv2.imshow("Motion Detection", frame)
-            
+
             # Update the previous frame
             prev_frame = current_frame
-            
+
             # Quit if the 'q' key is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
-                stop()
+                self.stop()
                 break
             print("end of iteration")
         print("destroying all windows and releasing cap")
         cv2.destroyAllWindows() # destroys cv2 window
         cap.release()
-        
 
-    
+
+
